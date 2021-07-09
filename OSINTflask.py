0d59d96eca99bb524bd5bcdf19f43613ce84b913
bertmad3400 <skrivtilbertram@gmail.com>
2021-07-09 11:06:30 +0200

    Added simple api point

--- OSINTflask.py
+++ OSINTflask.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 
+import psycopg2
+postgresqlPassword = ""
+
 from flask import Flask
 from flask import render_template
+from flask import send_from_directory
+
+from OSINTmodules import *
 import getOGTagsInHTML
 
 app = Flask(__name__)
 app.static_folder = "./static"
 app.template_folder = "./templates"
+
 
 @app.route('/')
 def selectUser():
@@ -17,6 +24,11 @@ def showFrontpage():
     HTML, CSS, JS = getOGTagsInHTML.main()
     return (render_template("feed.html", HTML=HTML, CSS=CSS, JS=JS))
 
+
+@app.route('/api')
+def api():
+    conn = psycopg2.connect("dbname=osinter user=postgres password=" + postgresqlPassword)
+    return OSINTdatabase.requestOGTagsFromDB(conn, 'articles')
 
 if __name__ == '__main__':
     app.run(debug=True)
