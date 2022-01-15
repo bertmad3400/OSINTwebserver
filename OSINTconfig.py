25787bd60b0000c8aa2b18d6533e60b5ce1ec704
bertmad3400 <skrivtilbertram@gmail.com>
2022-01-15 15:34:08 +0100

    Change user table name

--- OSINTconfig.py
+++ OSINTconfig.py
@@ -13,7 +13,7 @@ def loadSecretKey():
 
 class Config(object):
     DB_FILE_PATH = os.environ.get("DB_FILE_PATH") or "./osinter_users.db"
-    DB_USER_TABLE = os.environ.get('DB_USER_TABLE') or "osinter_users"
+    DB_USER_TABLE = os.environ.get('DB_USER_TABLE') or "users"
     ELASTICSEARCH_ARTICLE_INDEX = os.environ.get("ARTICLE_INDEX") or "osinter_articles"
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL') or "http://localhost:9200"
     SECRET_KEY = os.environ.get('SECRET_KEY') or loadSecretKey()
