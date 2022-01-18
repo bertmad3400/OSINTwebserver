3af01b9ccc98aa29cd1e9b42d828ba6868b5d232
bertmad3400 <skrivtilbertram@gmail.com>
2022-01-18 20:21:57 +0100

    Allow storage of elasticsearch cluster url in file

--- OSINTconfig.py
+++ OSINTconfig.py
@@ -11,10 +11,15 @@ def loadSecretKey():
             file.write(currentSecretKey)
         return currentSecretKey
 
+def loadElasticURL():
+    if os.path.isfile("./.elasticsearch.url"):
+        return Path("./.elasticsearch.url").read_text()
+    else:
+        return "http://localhost:9200"
 
 class Config(object):
     DB_FILE_PATH = os.environ.get("DB_FILE_PATH") or "./osinter_users.db"
     DB_USER_TABLE = os.environ.get('DB_USER_TABLE') or "users"
     ELASTICSEARCH_ARTICLE_INDEX = os.environ.get("ARTICLE_INDEX") or "osinter_articles"
-    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL') or "http://localhost:9200"
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL') or loadElasticURL()
     SECRET_KEY = os.environ.get('SECRET_KEY') or loadSecretKey()
